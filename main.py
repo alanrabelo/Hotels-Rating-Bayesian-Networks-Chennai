7bc352a5aaf52f7fce6643d4a05df3271ec8efaa
TiagoAle <tiago.alexandre@acad.ifce.edu.br>
2017-07-11 21:43:34 -0300

    Regular expressions and starting stop words

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import csv
+import re
+import nltk
+from nltk.corpus import stopwords
+
 
 with open('chennai_reviews.csv', 'r') as file:
     reader = csv.reader(file)
+    train = []
     for row in reader:
-        print(row)
-
+        row[2] = re.sub("[^a-zA-Z]",
+                              " ",
+                              row[2])
+        train.append(row)
+    titles = train[0]
+    train.remove(train[0])
+    print(titles)
+    print(train[5])
+    print stopwords.words("english")

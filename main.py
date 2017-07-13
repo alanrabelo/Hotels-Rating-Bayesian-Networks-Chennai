22560e09fb75a7b7733dd0013c95add1e7639869
alanrabelo <alanrabelo@icloud.com>
2017-07-12 23:19:56 -0300

    Trainning Complete :)

--- main.py
+++ main.py
@@ -1,42 +1,98 @@
 import csv
 import re
-import nltk
-from nltk.corpus import stopwords
+import numpy as np
 
 dictWords = {}
+global numberOfReviews
+global reviews
+global sentiments
+
 def main():
+
+
     with open('chennai_reviews.csv', 'r') as file:
         reader = csv.reader(file)
         train = []
+
+        global numberOfReviews
+        numberOfReviews = 0
+
+        global reviews
+        reviews = []
+
+        global sentiments
+        sentiments = []
+
         for row in reader:
-            row[2] = re.sub("[^a-zA-Z]",
-                              " ",
-                              row[2])
-            print(row[3])
+            numberOfReviews += 1
+            row[2] = re.sub("[^a-zA-Z]", " ", row[2])
             train.append(row)
+
         titles = train[0]
         train.remove(train[0])
-        print(titles)
-        print(train[1])
+
         for evaluation in train:
             countWords(evaluation[2], evaluation[3])
 
-        print(dictWords)
-        #print stopwords.words("english")
+
+        corrects = 0.0
+
+        for index,review in enumerate(reviews):
+            if classify(review) - int(sentiments[index]) == 0:
+                corrects += 1
+
+        print corrects / numberOfReviews
+
+def classify(sentence):
+    return np.argsort(probabilityOfSentenceBelongToSentiment(sentence))[2]
+
+def probabilityOfWordBelongToSentiment(word):
+
+    probabilities = [0,0,0]
+    global numberOfReviews
+
+    for i in range(0,3):
+        probabilities[i] = float(dictWords[word][i]) / numberOfReviews
+
+    return probabilities
+
+
+def probabilityOfSentenceBelongToSentiment(sentence):
+    probabilities = [1, 1, 1]
+    global numberOfReviews
+
+
+    for word in sentence:
+        wordprobabilities = probabilityOfWordBelongToSentiment(word)
+        for index,probability in enumerate(wordprobabilities):
+            probabilities[index] *= probability
+
+    return probabilities
+
+
 
 def countWords(words, sentiment):
-    array = words.split()
-    array = list(set(array))
-    #print(words)
+
+    if sentiment == "":
+        return
+
+    array = list(set(words.lower().split()))
+
+    global reviews
+    reviews.append(array)
+
+    global sentiments
+    sentimentindex = int(sentiment) - 1
+    sentiments.append(sentimentindex)
+
     for word in array:
         word.replace(" ", "")
         if not dictWords.has_key(word):
-            dictWords[word] = {"1":0, "2":0, "3":0}
-            dictWords[word][sentiment] = 1
+            dictWords[word] = [0, 0, 0]
+            dictWords[word][sentimentindex] = 1
         else:
-            #print(word)
-            #print(sentiment)
-            dictWords[word][sentiment] = dictWords[word][sentiment] + 1
+            dictWords[word][sentimentindex] += 1
+
 
 if __name__ == '__main__':
     main()
